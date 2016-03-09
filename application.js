4cf33e37c88d86cc0976a28fc6399bf956e39fa4
Clinton Halpin <clintonhalpin@gmail.com>
2016-03-08 21:26:11 -0500

    tweak: add caption class

--- application.js
+++ application.js
@@ -29,9 +29,9 @@ function render(){
     container.empty();
     for(var i = 0; i < places.length; i++) {
         var image = '<img src="'+ places[i].image +'" alt="...">';
-        var name = '<h1>'+ places[i].name +'</h1>';
+        var name = '<div class="caption"><h1>'+ places[i].name +'</h1>';
         var description = '<p>'+ places[i].name +'</p>';
-        var btnLike = '<a href="#" class="btn btn-primary btn-like" data-id="'+ i +'">Likes (' + places[i].likes + ')</a>';
+        var btnLike = '<a href="#" class="btn btn-primary btn-like" data-id="'+ i +'">Likes (' + places[i].likes + ')</a></div>';
         container.append('<div class="col-sm-6 col-md-4"><div class="thumbnail">' + image + name + description + btnLike + '</div></div>');
     }
 }

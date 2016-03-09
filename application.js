c1ce85c52159587357255a89b68d877c9e09c0d3
chalpin <chalpin@salesforce.com>
2016-03-09 08:51:27 -0500

    feat: Added more images and change like button text

--- application.js
+++ application.js
@@ -14,15 +14,45 @@ var places = [
         likes: 0
     },
     {
-        name: "California",
+        name: "New York",
         image: 'https://unsplash.it/400/300?image=349',
         likes: 0
     },
     {
-        name: "California",
+        name: "Utah",
         image: 'https://unsplash.it/400/300?image=432',
         likes: 0
-    }        
+    },
+    {
+        name: "Chicago",
+        image: 'https://unsplash.it/400/300?image=223',
+        likes: 0
+    },
+    {
+        name: "Pennsylvania",
+        image: 'https://unsplash.it/400/300?image=297',
+        likes: 0
+    },
+    {
+        name: "Berlin",
+        image: 'https://unsplash.it/400/300?image=444',
+        likes: 0
+    },
+    {
+        name: "Los Angeles",
+        image: 'https://unsplash.it/400/300?image=214',
+        likes: 0
+    },
+    {
+        name: "Iceland",
+        image: 'https://unsplash.it/400/300?image=512',
+        likes: 0
+    },
+    {
+        name: "Australia",
+        image: 'https://unsplash.it/400/300?image=1002',
+        likes: 0
+    }                      
 ];
 
 function render(){
@@ -30,9 +60,8 @@ function render(){
     for(var i = 0; i < places.length; i++) {
         var image = '<img src="'+ places[i].image +'" alt="...">';
         var name = '<div class="caption"><h1>'+ places[i].name +'</h1>';
-        var description = '<p>'+ places[i].name +'</p>';
-        var btnLike = '<a href="#" class="btn btn-primary btn-like" data-id="'+ i +'">Likes (' + places[i].likes + ')</a></div>';
-        container.append('<div class="col-sm-6 col-md-4"><div class="thumbnail">' + image + name + description + btnLike + '</div></div>');
+        var btnLike = '<a href="#" class="btn btn-primary btn-like" data-id="'+ i +'">&#10084; Likes ' + places[i].likes + '</a></div>';
+        container.append('<div class="col-sm-6 col-md-4"><div class="thumbnail">' + image + name + btnLike + '</div></div>');
     }
 }
 
@@ -49,7 +78,8 @@ $( document ).ready(function() {
 // hint. You'll have you manipulate the places array
 ///////////////////////////////////////
 
-$(document).on('click', '.btn-like', function() {
+$(document).on('click', '.btn-like', function(e) {
+    e.preventDefault();
     var index = $(this).attr('data-id');
     places[index].likes = places[index].likes + 1;
     render();

dfcc0cca3f02ebd7d00ef7f4dfbcdc712f8e1822
chalpin <chalpin@salesforce.com>
2016-03-09 08:52:35 -0500

    tweak: Change like button text

--- application.js
+++ application.js
@@ -30,9 +30,8 @@ function render(){
     for(var i = 0; i < places.length; i++) {
         var image = '<img src="'+ places[i].image +'" alt="...">';
         var name = '<div class="caption"><h1>'+ places[i].name +'</h1>';
-        var description = '<p>'+ places[i].name +'</p>';
-        var btnLike = '<a href="#" class="btn btn-primary btn-like" data-id="'+ i +'">Likes (' + places[i].likes + ')</a></div>';
-        container.append('<div class="col-sm-6 col-md-4"><div class="thumbnail">' + image + name + description + btnLike + '</div></div>');
+        var btnLike = '<a href="#" class="btn btn-primary btn-like" data-id="'+ i +'">&#10084; Likes ' + places[i].likes + '</a></div>';
+        container.append('<div class="col-sm-6 col-md-4"><div class="thumbnail">' + image + name + btnLike + '</div></div>');
     }
 }
 
